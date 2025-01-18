87ed7845916c094cd3f97214c64be2101cbdc84b
RMAICE <a.danon93@gmail.com>
2025-01-18 18:40:56 +0200

    fix: db connection string

--- libs/db.js
+++ libs/db.js
@@ -142,6 +142,6 @@ export function getNewDbConnection() {
   if (!process.env.DB_CONNECTION)
     throw new Error('No connection provided')
 
-  console.log(process.cwd() + path.resolve(process.env.DB_CONNECTION))
-  return new sqlite3.Database(process.cwd() + path.resolve(process.env.DB_CONNECTION))
+  console.log(path.resolve(process.env.DB_CONNECTION))
+  return new sqlite3.Database(path.resolve(process.env.DB_CONNECTION))
 }
